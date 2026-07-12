962424d56dbebe6118f3a3a1e216643bdc9935f6
Ryan970910 <ryan970910@gmail.com>
2026-07-12 22:58:33 +0800

    Refine Jianghu arena navigation and court

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,12 +22,6 @@ export default async function Home() {
           <strong>梦幻篮球</strong>
           <span>阵容编辑</span>
         </div>
-        <div className="jianghuNav" aria-label="赛季信息">
-          <span>赛季 2025-26</span>
-          <span>常规赛 第 28 轮</span>
-          <span>江湖令 1,250</span>
-          <span>梦幻分 12,450</span>
-        </div>
         <form action={logoutAction}>
           <button className="refreshButton" type="submit">退出</button>
         </form>
